e0279c60c60fce7f41a0532aa7bcb02c74ee8fdf
Arash <45104106+arashHaratian@users.noreply.github.com>
2021-03-20 00:59:11 +0330

    clean and comment the code

--- Chapter2/ten_armed_testbed.R
+++ Chapter2/ten_armed_testbed.R
@@ -1,18 +1,14 @@
-# TODO: refactor it! 
-# TODO: comments
+library(tidyverse)
 
-library(purrr)
-library(ggplot2)
 
 resample <- function(x, ...)
   x[sample.int(length(x), ...)]
 
-# making an k-armed bandits
-bandit <- function(k = 10, mean = 0, sd = 1, reward_baseline){
+# making an k-armed bandits environment
+bandit <- function(k = 10, mean = 0, sd = 1, reward_baseline = 0){
   # this function will keep the true rewards 
   #and a function will be returned for interacting with the bandits and receiving the reward.
   true_means <- rnorm(k, mean + reward_baseline, sd)
-  print(true_means)
   function(action, return_true_mean = F){
     reward <- rnorm(1, mean = true_means[action], sd = 1)
     if(return_true_mean)
@@ -23,32 +19,34 @@ bandit <- function(k = 10, mean = 0, sd = 1, reward_baseline){
 
 
 
-
-bandit_agent <- function(k = 10,
-                         eps = 0,
-                         steps = 1000,
-                         initial_action_value = 0,
-                         learning_function = learn_sample_average,
-                         step_size = 0.1,
-                         UCB_degree = NULL,
-                         add_to_reward = 0,
-                         is_gradient = F,
-                         is_gradient_baseline = F) {
-  
+# an agent for interacting with the bandits
+bandit_agent <- function(k = 10,  # number of arms
+                         eps = 0, # epsilon (prob of acting on random)
+                         steps = 1000, # number of times to interacting with one env
+                         initial_action_value = 0, # value of the Q
+                         learning_function = learn_sample_average,  # update rule function
+                         step_size = 0.1,  # constant step size in 'learn_step_size()' update rule (instead of sample average)
+                         UCB_degree = NULL, # 'C' in UCB (degree of exploration)
+                         add_to_reward = 0, # add a baseline to the true rewards
+                         is_gradient = F,   # using gradient bandit algorithm
+                         is_gradient_baseline = F # using average reward as a baseline in gradient
+                         ) {
+  # initializing the Q and the N
   Q <<- rep(initial_action_value, k)
   N <<- vector("integer", k)
-  rewards <- vector("double", steps)
-  actions <- vector("double", steps)
   
-  Qtemp <- rep(initial_action_value, k)
+  rewards <- vector("double", steps) # for storing the rewards of the actions
+  actions <- vector("double", steps) # for storing the count of the best action
   
-  prob <- runif(steps)
+  Qtemp <- rep(initial_action_value, k) # for storing the result of the UCB
   
-  bandits_instance <- bandit(k = 10, reward_baseline = add_to_reward)
-  best_action <- which.max(bandits_instance(1, TRUE)$true_means)
+  prob <- runif(steps) # prob of taking random action in each step (if prob < epsilon then explore)
+  
+  bandits_instance <- bandit(k = 10, reward_baseline = add_to_reward) # making an K-armed bandits environment
+  best_action <- which.max(bandits_instance(1, TRUE)$true_means)  # storing the best action of the environment
   
   for (step in seq_len(steps)) {
-    if(!is.null(UCB_degree)){
+    if(!is.null(UCB_degree)){ # if 'UCB_degree' specified then it will use the UCB
       time <- sum(N, 1) # == sum(N) + 1
       Qtemp <- Q + (UCB_degree * sqrt(log(time) / (N + 1e-5)))
       
@@ -58,7 +56,7 @@ bandit_agent <- function(k = 10,
       rewards[step] <- reward
       if(action == best_action)
         actions[step] <- 1
-    } else if(is_gradient){
+    } else if(is_gradient){ # if 'is_gradient == TRUE' then it will use the gradient based bandit algorithm
       Q_exp <- exp(Q)
       policy <- Q_exp / sum(Q_exp)
       action <- resample(seq_along(Q), 1, prob = policy)
@@ -76,7 +74,7 @@ bandit_agent <- function(k = 10,
         Q <<- Q + step_size * (reward) * (one_hot - policy)
       }
       
-    } else{
+    } else{ #if non of the above then the specified learning rule ('learn_sample_average()' or 'learn_step_size()') will be used
       action <- ifelse(prob[[step]] > eps, resample(which(Q == max(Q)), 1), resample(1:k, 1))
       reward <- bandits_instance(action)
       learning_function(action, reward, step_size)
@@ -89,176 +87,272 @@ bandit_agent <- function(k = 10,
 }
 
 
+# update the Q with sample average
 learn_sample_average <- function(action, reward, ...) {
+  # it should be passed as an argument to the agent function
   N[action] <<- N[action] + 1
   Q[action] <<- Q[action] + (1 / N[action] * (reward - Q[action]))
 }
 
+# update the Q with constant step size 
 learn_step_size <- function(action, reward, step_size, ...) {
+  # it should be passed as an argument to the agent
   N[action] <<- N[action] + 1
   Q[action] <<- Q[action] + (step_size * (reward - Q[action]))
 }
 
-# learn_gradient <- function(with_baseline = F, step_size, r, baseline){
-#   one_hot <- rep(0, length(Q))
-#   one_hot[action] <- 1
-#   if (with_baseline){
-#     Q <<- Q + step_size * (r - baseline) * (one_hot - policy)
-#   } else {
-#     Q <<- Q + step_size * (reward) * (one_hot - policy)
-#   }
-# }
 
 
 
 
 
-## figure 1
-n <- 10
-ten_bandits_instance <- bandit(k = n)
+
+## figure 2.1
+
+plot_fig2.1 <- function(n = 10, runs = 2000){
+  
+  bandits_instance <- bandit(k = n)
+  
+  mat <- matrix(nrow = runs, ncol = n)
+  for(action in seq_len(n))
+    for(i in seq_len(runs)){
+      mat[i, action] <- bandits_instance(action)
+    }
+  
+  true_means <- bandits_instance(1, T)$true_means
+  true_means_df <- data.frame(actions = seq_len(n), value = true_means)
+  df <- mat %>% 
+    as_tibble() %>% 
+    rename_with(~paste0("action", seq_len(n)), .cols = everything()) %>% 
+    pivot_longer(cols = everything(), names_to = "actions") %>% 
+    mutate(actions = as_factor(actions))
+  
+  plot <- ggplot(df) +
+    geom_violin(aes(x = actions, y = value, fill = actions)) +
+    geom_point(aes(x = actions, y = value), data = true_means_df)+
+    theme(axis.text.x = element_text(angle = 90)) + 
+    ylab("reward distribution")
+  
+  
+  return(plot)
+}
 
 
-mat <- matrix(nrow = 1000, ncol = n)
-for(action in seq_len(n))
-  for(i in 1:1000){
-    mat[i, action] <- ten_bandits_instance(action)
+## figure 2.2
+# reward
+
+plot_fig2.2.1 <- function(runs = 2000, steps = 1000) {
+  
+  mat1 <- mat2 <- mat3 <- matrix(nrow = runs , ncol = steps)
+  for(i in seq_len(runs)){
+    mat1[i, ] <- bandit_agent(eps = 0.01, steps = steps)$rewards
+    mat2[i, ] <- bandit_agent(eps = 0.1, steps = steps)$rewards
+    mat3[i, ] <- bandit_agent(eps = 0, steps = steps)$rewards
   }
-true_means <- ten_bandits_instance(1, T)$true_means
-true_means_df <- data.frame(action = seq_len(n), value = true_means)
-df <- as.data.frame(mat)%>%
-  tidyr::pivot_longer(cols = tidyr::everything(), names_to = "action")
-df$action <- forcats::fct_inorder(as.factor(df$action))
-ggplot(df, aes(x = action, y = value)) +
-  geom_violin() +
-  geom_linerange() +
-  geom_point(data = true_means_df)
+  
+  df <- tibble(
+    "eps0.01" = colMeans(mat1),
+    "eps0.1" = colMeans(mat2),
+    "eps0" = colMeans(mat3),
+    "steps" = seq_len(steps)
+  )
+  
+  plot <- ggplot(df, aes(x = steps)) +
+    geom_line(aes(y = eps0.01, color = "eps = 0.01")) +
+    geom_line(aes(y = eps0.1, color = "eps = 0.1")) +
+    geom_line(aes(y = eps0, color = "eps = 0")) +
+    ylab("average reward") +
+    scale_color_manual(breaks = c("eps = 0.1", "eps = 0.01", "eps = 0"), values = c("blue", "red", "darkgreen"))
+  
+  return(plot)
+}
 
 
-## figure 2
+#action
 
-#------- reward simple
-mat <- matrix(nrow = 2000 , ncol = 1000)
-for(i in 1:2000)
-  mat[i, ] <- bandit_agent( eps = 0.01, steps = 1000)$rewards
+plot_fig2.2.2 <- function(runs = 2000, steps = 1000) {
+  
+  mat1 <- mat2 <- mat3 <- matrix(nrow = runs , ncol = steps)
+  for(i in seq_len(runs)){
+    mat1[i, ] <- bandit_agent(eps = 0.01, steps = steps)$actions
+    mat2[i, ] <- bandit_agent(eps = 0.1, steps = steps)$actions
+    mat3[i, ] <- bandit_agent(eps = 0, steps = steps)$actions
+  }
+  
+  df <- tibble(
+    "eps0.01" = colMeans(mat1),
+    "eps0.1" = colMeans(mat2),
+    "eps0" = colMeans(mat3),
+    "steps" = seq_len(steps)
+  )
+  
+  plot <- ggplot(df, aes(x = steps)) +
+    geom_line(aes(y = eps0.01, color = "eps = 0.01")) +
+    geom_line(aes(y = eps0.1, color = "eps = 0.1")) +
+    geom_line(aes(y = eps0, color = "eps = 0")) +
+    ylab("% optimal action")+
+    scale_color_manual(breaks = c("eps = 0.1", "eps = 0.01", "eps = 0"), values = c("blue", "red", "darkgreen"))
+  
+  return(plot)
+}
 
-plot(colMeans(mat), type = "l", col = "red")
-
-mat2 <- matrix(nrow = 2000 , ncol = 1000)
-for(i in 1:2000)
-  mat2[i, ] <- bandit_agent( eps = 0.1, steps = 1000)$rewards
-lines(colMeans(mat2), type = "l", col = "blue")
-
-
-mat3 <- matrix(nrow = 2000 , ncol = 1000)
-for(i in 1:2000)
-  mat3[i, ] <- bandit_agent( eps = 0, steps = 1000)$rewards
-lines(colMeans(mat3), type = "l", col = "green")
-
-
-#---action
-mat2 <- matrix(nrow = 2000 , ncol = 1000)
-for(i in 1:2000)
-  mat2[i, ] <- bandit_agent( eps = 0.1, steps = 1000)$actions
-plot(colMeans(mat2), type = "l", col = "blue", ylim = c(0, 1))
-
-
-mat <- matrix(nrow = 2000 , ncol = 1000)
-for(i in 1:2000)
-  mat[i, ] <- bandit_agent( eps = 0.01, steps = 1000)$actions
-lines(colMeans(mat), type = "l", col = "red")
-
-
-mat3 <- matrix(nrow = 2000 , ncol = 1000)
-for(i in 1:2000)
-  mat3[i, ] <- bandit_agent( eps = 0, steps = 1000)$actions 
-lines(colMeans(mat3), type = "l", col = "green")
 
 
 ##figure 3
+plot_fig2.3 <- function(runs = 2000, steps = 1000) {
+  
+  
+  mat1 <- mat2 <- matrix(nrow = runs , ncol = steps)
 
-mat2 <- matrix(nrow = 2000 , ncol = 1000)
-for(i in 1:2000)
-  mat2[i, ] <- bandit_agent( eps = 0.1, steps = 1000, initial_action_value = 5, learning_function = learn_step_size)$actions
-plot(colMeans(mat2), type = "l", col = "blue")
+  for (i in seq_len(runs)){
+    mat1[i, ] <- bandit_agent(eps = 0, steps = steps, initial_action_value = 5, learning_function = learn_step_size)$actions
+    mat2[i, ] <- bandit_agent(eps = 0.1, steps = steps, learning_function = learn_step_size)$actions
+  }
 
-mat3 <- matrix(nrow = 2000 , ncol = 1000)
-for(i in 1:2000)
-  mat3[i, ] <- bandit_agent( eps = 0.1, steps = 1000, learning_function = learn_step_size)$actions 
-lines(colMeans(mat3), type = "l", col = "red")
-
-
+  df <- tibble(
+    "init5" = colMeans(mat1),
+    "eps0.1" = colMeans(mat2),
+    "steps" = seq_len(steps)
+  )
+  
+  plot <- ggplot(df, aes(x = steps)) +
+    geom_line(aes(y = init5, color = "init = 5, eps = 0")) +
+    geom_line(aes(y = eps0.1, color = "init = 0, eps = 0.1")) +
+    ylab("% optimal action") +
+    theme(legend.position="bottom")
+  
+  return(plot)
+}
 
 
 ##figure 4
 
-mat <- matrix(nrow = 2000 , ncol = 1000)
-for(i in 1:2000)
-  mat[i, ] <- bandit_agent(eps = 0, steps = 1000, UCB_degree = 2)$rewards
+plot_fig2.4 <- function(runs = 2000, steps = 1000) {
+  
+  
+  mat1 <- mat2 <- matrix(nrow = runs , ncol = steps)
+  for(i in seq_len(runs)){
+    mat1[i, ] <- bandit_agent(eps = 0, steps = steps, UCB_degree = 2)$rewards
+    mat2[i, ] <- bandit_agent( eps = 0.1, steps = steps)$rewards
+  }
 
-plot(colMeans(mat), type = "l", col = "red")
-
-mat2 <- matrix(nrow = 2000 , ncol = 1000)
-for(i in 1:2000)
-  mat2[i, ] <- bandit_agent( eps = 0.1, steps = 1000)$rewards
-lines(colMeans(mat2), type = "l", col = "blue")
-
+  df <- tibble(
+    "UCB" = colMeans(mat1),
+    "eps0.1" = colMeans(mat2),
+    "steps" = seq_len(steps)
+  )
+  
+  plot <- ggplot(df, aes(x = steps)) +
+    geom_line(aes(y = eps0.1, color = "eps = 0.1")) +
+    geom_line(aes(y = UCB, color =  "UCB")) +
+    scale_color_manual(breaks = c("UCB", "eps = 0.1"), values = c("darkblue", "lightgreen")) +
+    ylab("average reward")
+  
+  return(plot)
+}
 
 
 ##figure 5
 
-mat <- matrix(nrow = 2000 , ncol = 1000)
-for(i in 1:2000)
-  mat[i, ] <- bandit_agent(step_size = 0.1, is_gradient = T, is_gradient_baseline = T, add_to_reward = 4)$actions
-plot(colMeans(mat), type = "l", col = "red")
 
-mat2 <- matrix(nrow = 2000 , ncol = 1000)
-for(i in 1:2000)
-  mat2[i, ] <- bandit_agent(step_size = 0.1, is_gradient = T, is_gradient_baseline = F, add_to_reward = 4)$actions
-lines(colMeans(mat2), type = "l", col = "blue")
+plot_fig2.5 <- function(runs = 2000, steps = 1000) {
+  
+  mat1 <- mat2 <- mat3 <- mat4 <- matrix(nrow = runs , ncol = steps)
 
-mat3 <- matrix(nrow = 2000 , ncol = 1000)
-for(i in 1:2000)
-  mat3[i, ] <- bandit_agent(step_size = 0.4, is_gradient = T, is_gradient_baseline = T, add_to_reward = 4)$actions
-lines(colMeans(mat3), type = "l", col = "black")
+  for(i in seq_len(runs)){
+    mat1[i, ] <- bandit_agent(step_size = 0.1, is_gradient = T, is_gradient_baseline = T, add_to_reward = 4)$actions
+    mat2[i, ] <- bandit_agent(step_size = 0.1, is_gradient = T, is_gradient_baseline = F, add_to_reward = 4)$actions
+    mat3[i, ] <- bandit_agent(step_size = 0.4, is_gradient = T, is_gradient_baseline = T, add_to_reward = 4)$actions
+    mat4[i, ] <- bandit_agent(step_size = 0.4, is_gradient = T, is_gradient_baseline = F, add_to_reward = 4)$actions
+  }
 
-mat4 <- matrix(nrow = 2000 , ncol = 1000)
-for(i in 1:2000)
-  mat4[i, ] <- bandit_agent(step_size = 0.4, is_gradient = T, is_gradient_baseline = F, add_to_reward = 4)$actions
-lines(colMeans(mat4), type = "l", col = "green")
+  df <- tibble(
+    "a0.1b" = colMeans(mat1),
+    "a0.1" = colMeans(mat2),
+    "a0.4b" = colMeans(mat3),
+    "a0.4" = colMeans(mat4),
+    "steps" = seq_len(steps)
+  )
+  
+  plot <- ggplot(df, aes(x = steps)) +
+    geom_line(aes(y = a0.1b, color = "alpha = 0.1, with_baseline")) +
+    geom_line(aes(y = a0.1, color = "alpha = 0.1")) +
+    geom_line(aes(y = a0.4b, color = "alpha = 0.4, with_baseline")) +
+    geom_line(aes(y = a0.4, color = "alpha = 0.4")) +
+    scale_color_manual(
+      breaks = c(
+        "alpha = 0.1, with_baseline",
+        "alpha = 0.4, with_baseline",
+        "alpha = 0.1",
+        "alpha = 0.4"
+      ),
+      values = c("darkblue", "lightblue", "darkgreen", "green")) +
+    theme(legend.position="bottom") +
+    guides(color = guide_legend(ncol = 2, override.aes = list(size = 3))) +
+    labs(y = "% optimal action", subtitle = "the true rewards were sampled from a normal distribution\nwith a mean of +4 instead of zero")
+  
+  return(plot)
+}
 
 
 
 ##figure 6
-# params <- list(
-#   ,
-#   list("step_size" = 2 ^ (-5:1), "is_gradient" = T, "is_gradient_baseline" = T),
-#   list("UCB_degree" = 2 ^ (-4:2), eps = 0),
-#   list("initial_action_value" = 2 ^ (-2:2), "learning_function" = learn_step_size)
-# )
 
-params <- c(2 ^ (-7:-2))
+plot_fig2.6 <- function(runs = 2000, steps = 1000) {
+  epsilons <- c(2 ^ (-7:-2))
+  step_sizes <- c(2 ^ (-5:1))
+  UCB_degrees <- c(2 ^ (-4:2))
+  init_values <- c(2 ^ (-2:2))
+  
+  
+  mat1 <- matrix(nrow = runs , ncol = length(epsilons))
+  mat2 <- matrix(nrow = runs , ncol = length(step_sizes))
+  mat3 <- matrix(nrow = runs , ncol = length(UCB_degrees))
+  mat4 <- matrix(nrow = runs , ncol = length(init_values))
+  
+  for(i in seq_len(runs)){
+    mat1[i, ] <- colMeans(as.data.frame(map(epsilons, ~bandit_agent(eps = .x, steps = steps)$rewards)))
+    # plot(matrix(data = c( -7:-2, colMeans(mat1)), nrow = 6), type = "l", col = "red", xlim = c(-7, 2), ylim = c(1, 1.5))
+    df1 <- tibble("eps" = colMeans(mat1), "param" = -7:-2)
+    
+    mat2[i, ] <- colMeans(as.data.frame(map(step_sizes, ~bandit_agent(step_size = .x, is_gradient = T, is_gradient_baseline = T, steps = steps)$rewards)))
+    # lines(matrix(data = c( -5:1, colMeans(mat2)), nrow = 7), type = "l", col = "blue")
+    df2 <- tibble("step_size" = colMeans(mat2), "param" = -5:1)
+    
+    mat3[i, ] <- colMeans(as.data.frame(map(UCB_degrees, ~bandit_agent(UCB_degree = .x, eps = 0, steps = steps)$rewards)))
+    # lines(matrix(data = c(-4:2, colMeans(mat4)), nrow = 7), type = "l", col = "black")
+    df3 <- tibble("UCB" = colMeans(mat3), "param" = -4:2)
+    
+    mat4[i, ] <- colMeans(as.data.frame(map(init_values, ~bandit_agent(initial_action_value = .x, learning_function = learn_step_size, steps = steps)$rewards)))
+    # lines(matrix(data = c( -3:3, colMeans(mat4)), nrow = 7), type = "l", col = "green")
+    df4 <- tibble("init" = colMeans(mat4), "param" = -2:2)
+  }
+  
+  plot <- ggplot() +
+    geom_line(aes(x = param, y = eps, color = "epsilon greedy"), data = df1) +
+    geom_line(aes(x = param, y = step_size, color = "gradient bandit"), data = df2) +
+    geom_line(aes(x = param, y = UCB, color = "UCB"), data = df3) +
+    geom_line(aes(x = param, y = init, color = "optimistic initialization"), data = df4) +
+    scale_color_manual(
+      breaks = c(
+        "epsilon greedy",
+        "gradient bandit",
+        "UCB",
+        "optimistic initialization"
+      ),
+      values = c("red", "green", "darkblue", "black")) +
+    labs(x = bquote(~2^{parameters}), y = "average reward\nover first 1000 steps")
+  
+  
+  return(plot)
+}
 
-mat <- matrix(nrow = 2000 , ncol = 6)
-for(i in 1:2000)
-  mat[i, ] <- colMeans(map_dfc(params, ~bandit_agent(eps = .x)$rewards))
-plot(matrix(data = c( -7:-2, colMeans(mat)), nrow = 6), type = "l", col = "red", xlim = c(-7, 2), ylim = c(1, 1.5))
+# plotting the figures
 
-
-params <- c(2 ^ (-5:1))
-mat2 <- matrix(nrow = 2000 , ncol = 7)
-for(i in 1:2000)
-  mat2[i, ] <- colMeans(map_dfc(params, ~bandit_agent(step_size = .x, is_gradient = T, is_gradient_baseline = T)$rewards))
-lines(matrix(data = c( -5:1, colMeans(mat2)), nrow = 7), type = "l", col = "blue")
-
-params <- c(2 ^ (-4:2))
-mat3 <- matrix(nrow = 2000 , ncol = 8)
-for(i in 1:2000)
-  mat3[i, ] <- colMeans(map_dfc(params, ~bandit_agent(UCB_degree = .x, eps = 0)$rewards))
-lines(matrix(data = c(-4:2, colMeans(mat4)), nrow = 7), type = "l", col = "black")
-
-params <- c(2 ^ (-2:2))
-mat4 <- matrix(nrow = 2000 , ncol = 7)
-for(i in 1:2000)
-  mat4[i, ] <- colMeans(map_dfc(params, ~bandit_agent(initial_action_value = .x, learning_function = learn_step_size)$rewards))
-lines(matrix(data = c( -3:3, colMeans(mat4)), nrow = 7), type = "l", col = "green")
-
+plot_fig2.1()
+plot_fig2.2.1()
+plot_fig2.2.2()
+plot_fig2.3()
+plot_fig2.4()
+plot_fig2.5()
+plot_fig2.6()
