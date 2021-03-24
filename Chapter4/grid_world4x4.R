eda1a589f8fe9e8e97d7594bc65369be06a7a8c2
Arash <45104106+arashHaratian@users.noreply.github.com>
2021-03-24 11:24:32 +0430

    return best actions matrix

--- Chapter4/grid_world4x4.R
+++ Chapter4/grid_world4x4.R
@@ -60,33 +60,45 @@ compute_V <- function(discount = 1){
     }
     temp <- V
     V <- new_V
-    new_V <- V
+    new_V <- temp
   }
   return(new_V)
 }
 
 
 
-compute_V_star <- function(discount = 1){
+compute_V_star <- function(discount = 1, return_best_actions = F){
   V <- new_V <- matrix(0, nrow = rows, ncol = cols)
   V_k <- matrix(0, nrow = 1000, ncol = (rows*cols))
+  
+  best_actions <- matrix("T", nrow = rows, ncol = cols)
+  action_names <- names(actions)
   
   for(k in 1:1000){
     for(state in 2:((rows*cols) - 1)){
       vals <- map_dbl(actions, full_back, state = state, discount = discount, V = V)
       new_V[state] <- max(vals)
       V_k[k, ] <- V[state]
+      
+      act <- which(vals == max(vals))
+      best_actions[state] <- paste(action_names[act], collapse = "/")
     }
     temp <- V
     V <- new_V
-    new_V <- V
+    new_V <- temp
   }
+  
+  if(return_best_actions)
+    return(list("values" = V, "best_actions" = best_actions))
+  
   return(new_V)
 }
 
+
+
 compute_V()
 
-
+compute_V_star(return_best_actions = T)
 
 
 
