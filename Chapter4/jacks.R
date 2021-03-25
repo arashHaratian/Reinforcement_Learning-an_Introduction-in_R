ec1ca2dec09a45b4a3c57894a78434f6215c3d4d
Arash <45104106+arashHaratian@users.noreply.github.com>
2021-03-25 20:05:32 +0430

    fix the problem

--- Chapter4/jacks.R
+++ Chapter4/jacks.R
@@ -4,13 +4,6 @@ library(reshape2)
 
 V <- policy <- matrix(0, nrow = 21, ncol = 21)
 
-loc1 <- load_P_and_R(lambda_requests = 3, lambda_dropoffs = 3)
-P1 <- loc1$P
-R1 <- loc1$R
-
-loc2 <- load_P_and_R(lambda_requests = 4, lambda_dropoffs = 2)
-P2 <- loc2$P
-R2 <- loc2$R
 
 
 load_P_and_R <- function(lambda_requests, lambda_dropoffs){
@@ -20,7 +13,7 @@ load_P_and_R <- function(lambda_requests, lambda_dropoffs){
   while(TRUE){
     
     request_prob <- dpois(requests, lambda_requests)
-    R <- R + map_dbl(.x = 1:26, ~(request_prob * min(.x, requests) * 10))
+    R <- R + map_dbl(.x = 0:25, ~(request_prob * min(.x, requests) * 10))
     
     dropoffs <- 0
     while(TRUE){
@@ -48,6 +41,17 @@ load_P_and_R <- function(lambda_requests, lambda_dropoffs){
 
 
 
+
+loc1 <- load_P_and_R(lambda_requests = 3, lambda_dropoffs = 3)
+P1 <- loc1$P
+R1 <- loc1$R
+
+loc2 <- load_P_and_R(lambda_requests = 4, lambda_dropoffs = 2)
+P2 <- loc2$P
+R2 <- loc2$R
+
+
+
 backup_action <- function(n1, n2, action, discount = 0.9) {
   
   action <- min(action, n1) %>% 
@@ -56,17 +60,17 @@ backup_action <- function(n1, n2, action, discount = 0.9) {
     max(-5)
   
   morning_n1 <- n1 - action
-  # if(morning_n1 == 0)
-    # morning_n1 <- morning_n1 + 1
-  morning_n2 <- n2 + action
-  # if(morning_n2 == 0)
-    # morning_n2 <- morning_n2 + 1
+  if(morning_n1 <= 0)
+    return(0)
+  # morning_n1 <- 1
+
+  morning_n2 <- n2 + action 
+  if(morning_n2 <= 0)
+    return(0)
+    # morning_n2 <- 1
 
   vals1 <- vector("double", 21)
   vals2 <- vector("double", 21)
-  
-  ## TODO: map2_dbl
-  # map2_dbl(1:21, 1:21, ~ P1[morning_n1, .x] * P2[morning_n2, .y] * (R1[morning_n1] + R2[morning_n2] + discount * V[.x, .y]) 
   
   for(new_n1 in 1:21){ 
     for(new_n2 in 1:21){
@@ -83,7 +87,7 @@ backup_action <- function(n1, n2, action, discount = 0.9) {
 }
 
 
-policy_eval <- function(theta = 1e-4) { #TODO: 1e-7
+policy_eval <- function(theta = 1e-7) {
   while (TRUE) {
     
     old_v <- V
@@ -91,10 +95,9 @@ policy_eval <- function(theta = 1e-4) { #TODO: 1e-7
     for(n1 in 1:21){
       for(n2 in 1:21){
         action <- policy[n1, n2]
-        V[n1, n2] <<- backup_action(n1, n2, action) #TODO
+        V[n1, n2] <<- backup_action(n1, n2, action)
       }
     }
-    cat("\nmax change:", max(abs(old_v - V)))
     if(theta > max(abs(old_v - V)))
       break
   }
@@ -103,7 +106,7 @@ policy_eval <- function(theta = 1e-4) { #TODO: 1e-7
 
 
 
-policy_function <- function(n1, n2, eps = 0) {
+policy_function <- function(n1, n2) {
   actions <- max(-5, (-n2)):min(5, n1)
   action_vals <- map_dbl(actions, backup_action, n1 = n1, n2 = n2)
   return(actions[which.max(action_vals)])
@@ -132,23 +135,32 @@ policy_iteration <- function(){
   while(TRUE){
     policies[[as.character(count)]] <- policy
     policy_eval()
-    print(count)
-    count <- count + 1
     if(!greedify())
       break
+    count <- count + 1
   }
   return(policies)
 }
 
 
-# greedify()
-result <- policy_iteration()
 
-# backup_action(15, 5, 2)
+# plotting figure 4.2
 
-# ggplot(melt(V), aes(Var2, Var1)) + geom_tile(aes(fill = value)) +
-  # geom_text(aes(label = value)) +
-  # scale_fill_gradient2(low = "yellow", high = "blue") +
-#   labs(x = "",y = "")
-# # +
-#   # scale_y_reverse()
+plot_fig4.2 <- function(){
+  result <- policy_iteration()
+  
+  
+  plots <- map(.x = result,
+               ~ggplot(melt(.x), aes(Var2, Var1)) +
+                 geom_tile(aes(fill = value)) +
+                 labs(x = "#cars at second location",y = "#cars at first location"))
+  
+  
+  plots[["V"]] <- ggplot(melt(V), aes(Var2, Var1)) +
+    geom_tile(aes(fill = value)) +
+    labs(x = "#cars at second location",y = "#cars at first location")
+  
+  patchwork::wrap_plots(plots)
+}
+
+plot_fig4.2()
